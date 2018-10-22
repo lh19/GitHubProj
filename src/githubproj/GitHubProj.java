28c8300bb15e1fe359e7f5b84f5d0f07cce6f6c0
lh19 <>
2018-10-22 16:05:55 -0400

    This is the first commit

---
+++ src/githubproj/GitHubProj.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubproj;
+
+/**
+ *
+ * @author lh19
+ */
+public class GitHubProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hill();
+    }
+    
+    public static void Hill() {
+        System.out.println("Logan Hill");
+    }
+    
+}
